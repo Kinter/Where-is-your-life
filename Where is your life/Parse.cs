ad11417ebd1a75a44a9ed29bcf611c9ea8b334c5
Kinterh <in010210@gmail.com>
2018-04-09 20:07:08 +0900

    Add Parse.cs

---
+++ Where is your life/Parse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Where_is_your_life
+{
+    class Parse
+    {
+        
+    }
+}
