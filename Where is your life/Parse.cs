98dec52f5613a78101a0615c42711cd712ca7386
Kinterh <in010210@gmail.com>
2018-04-09 20:29:07 +0900

    생성자 추가

--- Where is your life/Parse.cs
+++ Where is your life/Parse.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace Where_is_your_life
 {
     class Parse
     {
-        
+        private string _username;
+        HtmlDocument mydoc = new HtmlDocument();
+
+        /// <summary>
+        /// 생성자임ㅋ
+        /// </summary>
+        /// <param name="username">You can find user name twitter.com/"here"</param>
+        Parse(string username)
+        {
+            _username = username;
+        }
+
+        public bool parseData()
+        {
+
+            return true;
+        }
     }
 }
