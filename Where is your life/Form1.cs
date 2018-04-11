43444197b9558a2224e263aba1650b2e9828308a
Kinterh <in010210@gmail.com>
2018-04-11 19:00:57 +0900

    기본 폼 추가

--- Where is your life/Form1.cs
+++ Where is your life/Form1.cs
@@ -12,9 +12,27 @@ namespace Where_is_your_life
 {
     public partial class Form1 : Form
     {
+        Parse _p;
+        string _username;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _username = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _p = new Parse(_username);
         }
     }
 }
