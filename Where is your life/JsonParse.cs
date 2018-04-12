093550fc308476a45b07bc05d645919317f7b30d
Kinterh <in010210@gmail.com>
2018-04-12 15:09:38 +0900

    기본 틀 구축

--- Where is your life/JsonParse.cs
+++ Where is your life/JsonParse.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Where_is_your_life
 {
     class JsonParse
     {
-
+        JObject jObject;
+        public JsonParse()
+        {
+            if(File.Exists("ParseData.json"))
+            
+            jObject = new JObject();
+        }
+        static public bool LoadData()
+        {
+            return true;
+        }
     }
 }
