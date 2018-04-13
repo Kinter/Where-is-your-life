bbf070a3b15646df56bca772c20e50de8981a3bf
Kinterh <in010210@gmail.com>
2018-04-13 15:18:48 +0900

    파일 존재 여부 확인

--- Where is your life/JsonParse.cs
+++ Where is your life/JsonParse.cs
@@ -5,25 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Where_is_your_life
 {
     class JsonParse
     {
         JObject jObject;
+        string _data;
+
+        public string Data { get => _data; private set => _data = value; }
+
+
         public JsonParse()
+        {
+            if(LoadData())
+            {
+
+            }
+        }
+
+
+        private bool LoadData()
         {
             if (File.Exists(System.Windows.Forms.Application.StartupPath + "ParseData.json"))
             {
+                Data = File.ReadAllText(System.Windows.Forms.Application.StartupPath + @"\ParseData.json");
 
             }
             else
             {
-                jObject = new JObject();
+                File.WriteAllText(System.Windows.Forms.Application.StartupPath + @"\ParseData.json", "0");
+                return true;
             }
-        }
-        static public bool LoadData()
-        {
             return true;
         }
     }
