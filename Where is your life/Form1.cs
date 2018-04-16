90b99e1bb1980280750055a79024000ec5c82d84
Kinterh <in010210@gmail.com>
2018-04-16 16:32:24 +0900

    addData에서 문자열 포맷 오류나서 다른거씀

--- Where is your life/Form1.cs
+++ Where is your life/Form1.cs
@@ -34,13 +34,15 @@ namespace Where_is_your_life
         private void button1_Click(object sender, EventArgs e)
         {
             _p = new HtmlParse(textBoxInput.Text);
-            _p.parseData();
+            _p.ParseData();
 
             textTweets.Text = _p.Tweets;
             textFollowers.Text = _p.Followers;
             textFollowings.Text = _p.Followings;
-
-            _j.AddData(_p.Username, Convert.ToInt32(_p.Tweets), Convert.ToInt32(_p.Followers), Convert.ToInt32(_p.Followings));
+            _j.AddData(_p.Username,
+                int.Parse(_p.Tweets, System.Globalization.NumberStyles.AllowThousands),
+                int.Parse(_p.Followers, System.Globalization.NumberStyles.AllowThousands),
+                int.Parse(_p.Followings, System.Globalization.NumberStyles.AllowThousands));
         }
 
         private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
