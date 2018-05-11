7567cd3dcffd93499620c9fc7d383ffc2087b79e
Kinterh <in010210@gmail.com>
2018-05-11 20:26:57 +0900

    기본 Ui 설정

--- Where is your life/Form_Settings.Designer.cs
+++ Where is your life/Form_Settings.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(30, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "컴퓨터 시작 시 자동 실행";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form_Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 209);
+            this.ClientSize = new System.Drawing.Size(452, 267);
+            this.Controls.Add(this.checkBox1);
             this.Name = "Form_Settings";
             this.Text = "Form_Settings";
             this.Load += new System.EventHandler(this.Form_Settings_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
