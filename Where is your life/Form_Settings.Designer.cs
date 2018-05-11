a9e0719779813bfdf23d726f7c1a2e0ea29f59bb
Kinterh <in010210@gmail.com>
2018-05-11 20:20:00 +0900

    Create Settings Form

---
+++ Where is your life/Form_Settings.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Where_is_your_life
+{
+    partial class Form_Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form_Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 209);
+            this.Name = "Form_Settings";
+            this.Text = "Form_Settings";
+            this.Load += new System.EventHandler(this.Form_Settings_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Where is your life/Form_Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Where_is_your_life
+{
+    public partial class Form_Settings : Form
+    {
+        public Form_Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Settings_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
